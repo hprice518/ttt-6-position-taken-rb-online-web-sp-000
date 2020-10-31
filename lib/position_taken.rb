a4ffeef87440783bd82dfc1d41704e71e5084ee6
Harrison Price <hprice518@gmail.com>
2020-10-31 23:38:13 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -8,10 +8,11 @@
 #end
 
 def position_taken?(board, index)
-  if board[index] == " " || if board[index] == "" || if board[index] == nil
+  if (board[index] == " ") || if (board[index] == "") || if (board[index] == nil)
     return false
   else
     return true  
   end 
 end
 
+def position_taken?(board, index)
