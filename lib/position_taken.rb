28b2ad7f179fbcc291d69327c04e55695e72ace0
Harrison Price <hprice518@gmail.com>
2020-10-31 23:19:30 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,16 +1,8 @@
 
-#def position_taken?(board, input)
-#  if input != "X" || input != "O"
-#    return FALSE 
-#  else board[X] || board[O]
-#    return TRUE
-#  end
-#end
-
-def position_taken?(board, index)
-   if (board[index] == " ") || (board[index] == "") || (board[index] == nil)
-      return false 
-   else
-      return true
-   end
+def position_taken?(board, input)
+  if input != "X" || input != "O"
+    return FALSE 
+  else board[X] || board[O]
+    return TRUE
+  end
 end
